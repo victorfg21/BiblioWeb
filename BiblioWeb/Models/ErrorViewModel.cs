73b47c56694006e84705c487d5b7e7b465099c5d
victorfg21 <32532774+victorfg21@users.noreply.github.com>
2018-05-24 22:19:54 -0300

    Adicionar arquivos de projeto.

---
+++ BiblioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BiblioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
